3390dd065e3e8e4d6b4f43989eb4ec3130fb00eb
Tazim381 <jucse28.381@gmail.com>
2023-02-03 19:53:01 +0600

    calculator maked

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Calculator from './calculator/calculator.jsx';
+function App() {
+  return (
+    <div className="Calculator">
+     <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/calculator/calculator.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import '../App.css';
+
+
+function Calculator() {
+ 
+ const[count,setValue] =useState();
+ const[count1,setValue1] = useState();
+ const[answer,result] = useState();
+ function value1(event) {
+  setValue(event.target.value);
+ }
+ function value2(event) {
+  setValue1(event.target.value);
+ }
+ function add() {
+    result(parseInt(count)+ parseInt(count1));
+ }
+ function substract() {
+    result(parseInt(count)-parseInt(count1));
+ }
+ function multiply() {
+    result(parseInt(count)* parseInt(count1));
+ }
+ function division() {
+    result(parseInt(count)/ parseInt(count1));
+    console.log("clicked");
+ }
+
+  return <div className="counter-wrap">
+    <h1>Calculator  App</h1>
+    <input onChange={value1} type="number" value={count}></input>
+    <input onChange={value2} type="number" value={count1}></input>
+    <button onClick={add}>Add</button>
+    <button onClick={substract}>Substract </button>
+    <button onClick={multiply} >Multiply</button>
+    <button onClick={division}>Division</button>
+    <p>{`Result  = ${answer}`}</p>
+  </div>;
+}
+ 
+export default Calculator;
